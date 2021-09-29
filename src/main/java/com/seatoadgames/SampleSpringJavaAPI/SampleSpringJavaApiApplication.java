243371c0b01583c07f80febee6c798184e659be4
Edmond Szeto <eszeto831@gmail.com>
2021-09-29 00:57:20 -1000

    test app

---
+++ src/main/java/com/seatoadgames/SampleSpringJavaAPI/SampleSpringJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.seatoadgames.SampleSpringJavaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringJavaApiApplication.class, args);
+	}
+
+}
